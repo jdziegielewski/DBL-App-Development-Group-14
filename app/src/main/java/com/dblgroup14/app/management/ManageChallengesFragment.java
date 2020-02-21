523e6a0c2c3ceb2b104e41ab248acf9ecb344ac8
Jens Coenders <jens@famcoenders.nl>
2020-02-21 11:51:19 +0100

    Add EditActivity. Reorganize packages.

--- app/src/main/java/com/dblgroup14/app/management/ManageChallengesFragment.java
+++ app/src/main/java/com/dblgroup14/app/management/ManageChallengesFragment.java
@@ -1,4 +1,4 @@
-package com.dblgroup14.app.ui.management;
+package com.dblgroup14.app.management;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,10 +7,10 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import com.dblgroup14.app.R;
 
-public class ManageUserFragment extends Fragment {
+public class ManageChallengesFragment extends Fragment {
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_manage_user, container, false);
+        return inflater.inflate(R.layout.fragment_manage_challenges, container, false);
     }
 }
--- app/src/main/java/com/dblgroup14/app/management/ManageUserFragment.java
+++ app/src/main/java/com/dblgroup14/app/management/ManageUserFragment.java
@@ -1,4 +1,4 @@
-package com.dblgroup14.app.ui.score;
+package com.dblgroup14.app.management;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,10 +7,10 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import com.dblgroup14.app.R;
 
-public class ScoreFragment extends Fragment {
+public class ManageUserFragment extends Fragment {
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_score, container, false);
+        return inflater.inflate(R.layout.fragment_manage_user, container, false);
     }
 }
