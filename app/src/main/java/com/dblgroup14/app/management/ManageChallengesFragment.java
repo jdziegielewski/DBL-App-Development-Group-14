ad0740c75f3b6860509159a7e9e09ca18ae693fc
Riviom <calin.botoroaga@gmail.com>
2020-03-12 22:48:23 +0100

    Added temporary button to ManageChallenges Page to redirect to Challenge area

--- app/src/main/java/com/dblgroup14/app/management/ManageChallengesFragment.java
+++ app/src/main/java/com/dblgroup14/app/management/ManageChallengesFragment.java
@@ -5,9 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.widget.Button;
 import androidx.fragment.app.Fragment;
+import com.dblgroup14.app.PlayChallengePage;
 import com.dblgroup14.app.R;
 import com.dblgroup14.app.challenges.challenge1;
 import com.dblgroup14.app.challenges.shakechallenge;
@@ -19,6 +19,8 @@ public class ManageChallengesFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_manage_challenges, container, false);
     }
     
+    private Button toChallengePageButton;
+    
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -29,12 +31,15 @@ public class ManageChallengesFragment extends Fragment {
                 Intent challengesIntent = new Intent(getContext(), challenge1.class);
                 startActivity(challengesIntent);
             }
+            
         });
-        view.findViewById(R.id.goToShakeITChallenge).setOnClickListener(new View.OnClickListener() {
+        toChallengePageButton = (Button) getActivity().findViewById(R.id.tochallengebutton);
+       
+        toChallengePageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent challengesIntent = new Intent(getContext(), shakechallenge.class);
-                startActivity(challengesIntent);
+                Intent myIntent = new Intent(getActivity().getBaseContext(), PlayChallengePage.class);
+                startActivity(myIntent);
             }
         });
     }
