c4a0f7305593edbab037b7c82c612b47d8eb4d37
Mihai-Dragos <dragos200019@yahoo.com>
2020-03-20 08:40:06 +0100

    Add Login Page

--- app/src/main/java/com/dblgroup14/app/management/ManageUserFragment.java
+++ app/src/main/java/com/dblgroup14/app/management/ManageUserFragment.java
@@ -1,16 +1,41 @@
 package com.dblgroup14.app.management;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.dblgroup14.app.R;
+import com.dblgroup14.app.user_login.Login;
+import com.dblgroup14.app.user_login.Register;
 
 public class ManageUserFragment extends Fragment {
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_manage_user, container, false);
+    }
+    
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        
+        view.findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent challengesIntent = new Intent(getContext(), Login.class);
+                startActivity(challengesIntent);
+            }
+        });
+        view.findViewById(R.id.registerButton23).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent challengesIntent = new Intent(getContext(), Register.class);
+                startActivity(challengesIntent);
+            }
+        });
     }
 }
