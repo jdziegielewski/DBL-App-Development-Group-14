b6956560d1d1ee3376bbea05948fb70abab1864f
Teddabeertje <50151141+Teddabeertje@users.noreply.github.com>
2020-03-25 14:56:10 +0100

    Edit alarm is now scrollable

--- app/src/main/java/com/dblgroup14/app/management/CustomListAdapter.java
+++ app/src/main/java/com/dblgroup14/app/management/CustomListAdapter.java
@@ -5,31 +5,22 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.annotation.RequiresApi;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import com.dblgroup14.app.AlarmActivity;
 import com.dblgroup14.app.R;
-import com.dblgroup14.app.challenges.challenge1;
 import com.dblgroup14.app.management.edit.EditActivity;
 import com.dblgroup14.support.AppDatabase;
 import com.dblgroup14.support.entities.Alarm;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.TimeZone;
 
 //https://appsandbiscuits.com/listview-tutorial-android-12-ccef4ead27cc
 public class CustomListAdapter extends ArrayAdapter<Alarm> {
@@ -57,29 +48,29 @@ public class CustomListAdapter extends ArrayAdapter<Alarm> {
         
         alarmTimeTextView.setText(String.format("%s:%s", hours, min));
         nameTextView.setText(alarm.name);
-    
-        AlarmManager alarmMgr = (AlarmManager)activity.getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+        
+        AlarmManager alarmMgr = (AlarmManager) activity.getApplicationContext().getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(activity.getBaseContext(), AlarmActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), alarm.id, intent, 0);
         
         ImageView deleteView = rowView.findViewById(R.id.deleteAlarmView);
         deleteView.setOnClickListener(view1 -> {
             alarm.setEnabled(false);
-            if (alarmMgr!= null) {
+            if (alarmMgr != null) {
                 alarmMgr.cancel(pendingIntent);
             }
             AsyncTask.execute(() -> {
                 AppDatabase.db().alarmDao().delete(alarm);
             });
         });
-    
+        
         ConstraintLayout dropDownView = rowView.findViewById(R.id.dropDownView);
         ConstraintLayout mainBoxAlarm = rowView.findViewById(R.id.mainBoxAlarm);
         dropDownView.setVisibility(View.GONE);
         mainBoxAlarm.setOnClickListener(view12 -> {
-            if(dropDownView.getVisibility() == rowView.VISIBLE){
+            if (dropDownView.getVisibility() == rowView.VISIBLE) {
                 dropDownView.setVisibility(View.GONE);
-            } else if(dropDownView.getVisibility() == rowView.GONE){
+            } else if (dropDownView.getVisibility() == rowView.GONE) {
                 dropDownView.setVisibility(View.VISIBLE);
             }
         });
@@ -93,7 +84,7 @@ public class CustomListAdapter extends ArrayAdapter<Alarm> {
         });
         
         ImageView alarmOnOffView = rowView.findViewById(R.id.alarmOnOffView);
-        if(alarm.enabled){
+        if (alarm.enabled) {
             alarmOnOffView.setBackgroundResource(R.drawable.ic_alarm_on);
         } else {
             alarmOnOffView.setBackgroundResource(R.drawable.ic_alarm_off);
@@ -101,13 +92,14 @@ public class CustomListAdapter extends ArrayAdapter<Alarm> {
         
         alarmOnOffView.setOnClickListener(view14 -> {
             
-            if(alarmOnOffView.getBackground().getConstantState() == activity.getResources().getDrawable(R.drawable.ic_alarm_on).getConstantState()){
+            if (alarmOnOffView.getBackground().getConstantState() == activity.getResources().getDrawable(R.drawable.ic_alarm_on).getConstantState()) {
                 alarmOnOffView.setBackgroundResource(R.drawable.ic_alarm_off);
                 alarm.setEnabled(false);
-                if (alarmMgr!= null) {
+                if (alarmMgr != null) {
                     alarmMgr.cancel(pendingIntent);
                 }
-            } else if(alarmOnOffView.getBackground().getConstantState() == activity.getResources().getDrawable(R.drawable.ic_alarm_off).getConstantState()){
+            } else if (alarmOnOffView.getBackground().getConstantState() ==
+                    activity.getResources().getDrawable(R.drawable.ic_alarm_off).getConstantState()) {
                 alarmOnOffView.setBackgroundResource(R.drawable.ic_alarm_on);
                 alarm.setEnabled(true);
                 
@@ -116,8 +108,8 @@ public class CustomListAdapter extends ArrayAdapter<Alarm> {
                 cal.set(Calendar.HOUR_OF_DAY, alarm.hours);
                 cal.set(Calendar.MINUTE, alarm.minutes);
                 cal.set(Calendar.SECOND, 0);
-    
-                if(cal.compareTo(currentTime) > 0) {
+                
+                if (cal.compareTo(currentTime) > 0) {
                     assert alarmMgr != null;
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                         alarmMgr.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
