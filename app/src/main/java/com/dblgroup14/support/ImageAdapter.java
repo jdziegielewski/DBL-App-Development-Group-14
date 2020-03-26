71b9f55ebd512f77260427144bda12d5d7b53250
Riviom <calin.botoroaga@gmail.com>
2020-03-26 17:43:53 +0100

    2nd try imgAdapter

--- app/src/main/java/com/dblgroup14/support/ImageAdapter.java
+++ app/src/main/java/com/dblgroup14/support/ImageAdapter.java
@@ -8,44 +8,51 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import com.dblgroup14.app.R;
 
-public class ImageAdapter extends BaseAdapter {
+
+public class ImageAdapter extends BaseAdapter
+{
     private Context mContext;
-    
-    public ImageAdapter(Context c) {
+    public ImageAdapter(Context c)
+    {
         mContext = c;
     }
     
-    public int getCount() {
+    public int getCount()
+    {
         return mThumbIds.length;
     }
     
-    public Object getItem(int position) {
+    public Object getItem(int position)
+    {
         return null;
     }
     
-    public long getItemId(int position) {
+    public long getItemId(int position)
+    {
         return 0;
     }
-    
-    
-    public View getView(int position, View convertView, ViewGroup parent) {
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
         ImageView imageView;
-        if (convertView == null) {
+        if (convertView == null)
+        {
             imageView = new ImageView(mContext);
             imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
-        } else {
+            imageView.setPadding(6, 6, 6, 6);
+        }
+        else
+        {
             imageView = (ImageView) convertView;
         }
-        
         imageView.setImageResource(mThumbIds[position]);
         return imageView;
     }
-    
-    // references to our images
     private Integer[] mThumbIds = {
-            R.drawable.ic_add_circle_dp_black, R.drawable.math_equation_logo,
-           
+            R.drawable.math_equation_logo,
+            R.drawable.barcode_challenge,
+            R.drawable.ic_add_circle_outline_black_24dp
     };
 }
+
