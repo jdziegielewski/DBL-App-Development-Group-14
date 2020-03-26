c1b9c6991681bf991d645bea6e6f3657f605dd99
Riviom <calin.botoroaga@gmail.com>
2020-03-26 17:30:54 +0100

    1st try imgAddaptor

---
+++ app/src/main/java/com/dblgroup14/support/ImageAdapter.java
@@ -0,0 +1,51 @@
+package com.dblgroup14.support;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import com.dblgroup14.app.R;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+    
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+    
+    public int getCount() {
+        return mThumbIds.length;
+    }
+    
+    public Object getItem(int position) {
+        return null;
+    }
+    
+    public long getItemId(int position) {
+        return 0;
+    }
+    
+    
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+        
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+    
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.ic_add_circle_dp_black, R.drawable.math_equation_logo,
+           
+    };
+}
