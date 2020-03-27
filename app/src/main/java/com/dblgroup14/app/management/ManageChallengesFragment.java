3c83741b7799deb4673815201d1020fadf5055c1
Teddabeertje <50151141+Teddabeertje@users.noreply.github.com>
2020-03-27 15:20:59 +0100

    Added top tabs to the challenge management page

--- app/src/main/java/com/dblgroup14/app/management/ManageChallengesFragment.java
+++ app/src/main/java/com/dblgroup14/app/management/ManageChallengesFragment.java
@@ -1,13 +1,20 @@
 package com.dblgroup14.app.management;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import androidx.viewpager.widget.ViewPager;
 import com.dblgroup14.app.R;
 import com.dblgroup14.app.management.ui.main.SectionsPagerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
@@ -23,13 +30,13 @@ public class ManageChallengesFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getActivity().setContentView(R.layout.challenge_fragment_tab1);
+        //getActivity().setContentView(R.layout.challenge_fragment_tab1);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getContext(), getActivity().getSupportFragmentManager());
         ViewPager viewPager = getActivity().findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = getActivity().findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = getActivity().findViewById(R.id.fab);
         
     }
+    
 }
